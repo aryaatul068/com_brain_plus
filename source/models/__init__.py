7a2ce8d0481aaff2376455fb44e80fd8ce5549b6
aryaatul068 <aryaatul068@gmail.com>
2025-06-17 14:53:57 +0530

    Minor change

--- source/models/__init__.py
+++ source/models/__init__.py
@@ -3,9 +3,10 @@ from omegaconf import DictConfig
 from .brainnetcnn import BrainNetCNN
 from .fbnetgen import FBNETGEN
 from .COMTF import ComBrainTF
+from .COMTF import ComBrainTFPlus
 
 
 def model_factory(config: DictConfig):
     if config.model.name in ["LogisticRegression", "SVC"]:
         return None
-    return eval(config.model.name)(config).cuda()
+    return eval(config.model.name)(config).cpu()
